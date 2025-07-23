856625fe85e9971987bec8e382e25093e466f573
hannie1126 <hannie110311@gmail.com>
2025-07-23 15:13:28 +0900

    개념 콘텐츠 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/pages/LoginPages/LoginView.vue'
+// import LoginView from '@/pages/LoginPages/LoginView.vue'
 import LoginFormPage from '@/pages/LoginPages/LoginFormPage.vue'
-import SignupFormPage from '@/pages/SignupFormPage.vue'
+import SignupFormPage from '@/pages/LoginPages/SignupFormPage.vue'
 import ChatBotPage from '@/pages/ChatBotPage.vue'
 import FindAccountPage from '@/pages/LoginPages/FindAccountPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import InvestmentTestPage from '@/pages/InvestmentTestPage/InvestMentTestPage.vue'
+import LearningDetailPage from '@/pages/Learning/LearningDetailPage.vue';
+import LearningQuizPage from '@/pages/Learning/LearningQuizPage.vue';
+import LearningPage from '@/pages/Learning/LearningPage.vue';
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Login',
+  //   component: LoginView,
+  // },
   {
     path: '/',
-    name: 'Login',
-    component: LoginView,
+    name: 'LoginFormPage',
+    component: LoginFormPage,
   },
   {
     path: '/login-form',
@@ -44,6 +52,21 @@ const routes = [
     component: InvestmentTestPage,
   },
   // 나중에 /home, /mypage 등 추가 가능
+     {
+      path: '/learning',
+      name: 'learning',
+      component: LearningPage,
+    },
+    {
+      path: '/learning/:id',
+      name: 'learning-detail',
+      component: LearningDetailPage,
+    },
+    {
+      path: '/learning/:id/quiz',
+      name: 'learning-quiz',
+      component: LearningQuizPage,
+    },
 ]
 
 const router = createRouter({
