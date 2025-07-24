a305ad05d3ad0e4f6171e466744d7222c3bff81e
hannie1126 <hannie110311@gmail.com>
2025-07-24 20:48:53 +0900

    작업 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div class="mobile-container">
-      <router-view />
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
     </div>
   </div>
 </template>
