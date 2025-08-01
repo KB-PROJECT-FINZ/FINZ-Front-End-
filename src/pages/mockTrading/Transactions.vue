5b914ad76ac9c7f8a0782dcbfa14acc7097936aa
Shin <tlsgudwns010@naver.com>
2025-08-01 10:39:54 +0900

    refactor : 불필요한 log/로직 제거, api 호출 문제 해결

--- src/pages/mockTrading/Transactions.vue
+++ src/pages/mockTrading/Transactions.vue
@@ -320,8 +320,8 @@ const getStockInitial = (stockName) => {
   if (/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(stockName.charAt(0))) {
     return stockName.charAt(0)
   }
-  // 영문의 경우 첫 두 글자 사용
-  return stockName.substring(0, 2).toUpperCase()
+  // 영문의 경우도 첫 글자 사용
+  return stockName.substring(0, 1).toUpperCase()
 }
 
 // 이미지 로딩 에러 처리
