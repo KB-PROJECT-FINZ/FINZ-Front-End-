a952c56dcf9b77d2c20ce1acc0f9ff3fc31ececd
Shin <tlsgudwns010@naver.com>
2025-08-09 13:43:44 +0900

    fix : 사용하지 않는 import 제거

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Vue3Lottie from 'vue3-lottie'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(Vue3Lottie)
 app.use(createPinia())
 app.use(router)
 
